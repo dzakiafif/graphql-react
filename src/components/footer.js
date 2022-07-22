7087a724c76f9186ab191889655511e653e7c0fa
intan irnanda <45584774+intanirnanda5796@users.noreply.github.com>
2022-07-22 12:33:10 +0700

    edit dynamic content, btn load more and fotter index

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,7 +9,7 @@ function Footer(){
     }
 
     return(
-        <div className={darkMode ? "md:hidden flex px-4 py-2 fixed bottom-0 inset-x-0 z-50 bg-gray-800 flex-row space-x-20 justify-center" : "md:hidden flex px-4 py-2 fixed bottom-0 inset-x-0 z-50 bg-blue-800 flex-row space-x-20 justify-center"}>
+        <div className={darkMode ? "md:hidden flex px-4 py-2 fixed bottom-0 inset-x-0 z-10 bg-gray-800 flex-row space-x-20 justify-center" : "md:hidden flex px-4 py-2 fixed bottom-0 inset-x-0 z-50 bg-blue-800 flex-row space-x-20 justify-center"}>
             <div className="flex flex-col space-y-1 place-items-center">
                 <Icon icon="ant-design:home-filled" className="text-white text-center" width={25}/>
                 <h1 className="font-poppins text-white text-xs">Homepage</h1>
