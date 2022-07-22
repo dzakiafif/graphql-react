ac43ef4a8889e80cc62265004f3ac3c9dc79ec36
dzakiafif <dzakiafif12@gmail.com>
2022-07-22 15:58:34 +0700

    adding infinite scroll

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
+import InfiniteScroll from "react-infinite-scroller";
 import { LIST_ANIME } from "../../graphql/queries";
 
 function Homepage() {
@@ -11,55 +12,61 @@ function Homepage() {
   });
 
   const handleLoadMore = () => {
-    data?.Page.pageInfo.hasNextPage ? fetchMore({
-      variables: {
+    data?.Page.pageInfo.hasNextPage &&
+      fetchMore({
+        variables: {
           page: data?.Page.pageInfo.currentPage + 1,
-          perPage: 10
-      },
-      updateQuery: (prevResult, { fetchMoreResult }) => {
+          perPage: 10,
+        },
+        updateQuery: (prevResult, { fetchMoreResult }) => {
           fetchMoreResult.Page.media = [
-              ...prevResult.Page.media,
-              ...fetchMoreResult.Page.media
+            ...prevResult.Page.media,
+            ...fetchMoreResult.Page.media,
           ];
 
           return fetchMoreResult;
-      }
-  }) : setHide(true);
-  }
+        },
+      });
+  };
 
   return (
     <>
-      <div className="pt-10 pb-20 px-20 grid grid-cols-1 lg:grid-cols-4 place-items-center space-y-5">
-        {data?.Page.media.map((val, i) => (
-          <div
-            onClick={() => setShowModalDetail(true)}
-            className="bg-gray-100 px-4 py-4 rounded-xl"
-            key={i}
-          >
-            <img
-              className="rounded-md w-64 h-96 md:w-52 md:h-72"
-              src={val.coverImage.large}
-              alt=""
-            />
-            <h1 className="pt-2 text-base font-poppins font-semibold text-gray-800 w-44">
-              {val.title.romaji.slice(0,17) + '...'}
-            </h1>
-            <h1 className="font-poppins font-medium text-gray-500 text-xs">
-              {val.title.native.slice(0,17) + '...' }
-            </h1>
-            <h1 className="pt-3 font-poppins font-semibold text-sm text-right">
-              {`Popularity: ${new Intl.NumberFormat('id', {
-                        minimumFractionDigits: 0,
-                        maximumFractionDigits: 2,
-                      })
-                        .format(val.popularity)} viewers`}
-            </h1>
-          </div>
-        ))}
-      </div>
-      
-      <div className={`flex justify-center pb-8 ${hide ? 'invisible' : 'visible'}`}><button onClick={() => handleLoadMore()} className="bg-blue-600 px-6 rounded-md text-white font-poppins py-2 hover:bg-blue-800">Load More</button></div>
-      
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={handleLoadMore}
+        hasMore={data?.Page.pageInfo.hasNextPage}
+        loader={<h3>loading...</h3>}
+      >
+        <div className="pt-10 pb-20 px-20 grid grid-cols-1 lg:grid-cols-4 place-items-center space-y-5">
+          {data?.Page.media.map((val, i) => (
+            <div
+              onClick={() => setShowModalDetail(true)}
+              className="bg-gray-100 px-4 py-4 rounded-xl"
+              key={i}
+            >
+              <img
+                className="rounded-md w-64 h-96 md:w-52 md:h-72"
+                src={val.coverImage.large}
+                alt=""
+              />
+              <h1 className="pt-2 text-base font-poppins font-semibold text-gray-800 w-44">
+                {val.title.romaji.slice(0, 17) + "..."}
+              </h1>
+              <h1 className="font-poppins font-medium text-gray-500 text-xs">
+                {val.title.native.slice(0, 17) + "..."}
+              </h1>
+              <h1 className="pt-3 font-poppins font-semibold text-sm text-right">
+                {`Popularity: ${new Intl.NumberFormat("id", {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 2,
+                }).format(val.popularity)} viewers`}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
+
+      {/* <div className={`flex justify-center pb-8 ${hide ? 'invisible' : 'visible'}`}><button onClick={() => handleLoadMore()} className="bg-blue-600 px-6 rounded-md text-white font-poppins py-2 hover:bg-blue-800">Load More</button></div> */}
 
       {/* Modal detail */}
       {showModalDetail ? (
