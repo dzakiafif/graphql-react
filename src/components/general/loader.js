a49f82db55ff723ce332639ccf54e66b1a704388
intanirnanda5796 <intan.irnanda@gmail.com>
2022-07-25 16:39:41 +0700

    convert loader and label to styled components

--- src/components/general/loader.js
+++ src/components/general/loader.js
@@ -22,11 +22,6 @@ const Loader = () => {
                 <FirstBouncing hasCircle />
                 <SecondBouncing hasCircle />
                 <ThirdBouncing hasCircle />
-            {/* <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
-                <div
-                    className={`${circleCommonClasses} mr-1 animate-bounce200`}
-                ></div>
-                <div className={`${circleCommonClasses} animate-bounce400`}></div> */}
             </MainGroup>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -195,6 +195,7 @@ module.exports = {
         '"Courier New"',
         'monospace',
       ],
+      'poppins': ['Poppins', 'sans-serif'] 
     },
     fontSize: {
       xs: ['0.75rem', { lineHeight: '1rem' }],
